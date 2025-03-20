2de1c7ef3912efc53eca3f9f62e09942741d0deb
Priyobroto Kar <priyobrotokar@gmail.com>
2025-03-20 12:47:10 +0530

    feat: implemented all features

---
+++ index.js
@@ -0,0 +1,79 @@
+const url = "https://api.freeapi.app/api/v1/public/quotes/quote/random";
+const quoteContainer = document.getElementById("quote");
+const quoteText = document.getElementById("quote-text");
+const quoteAuthor = document.getElementById("author");
+const quoteImage = document.getElementById("quote-img");
+const quoteImageContainer = document.querySelector("#quote div");
+const nextQuoteBtn = document.getElementById("next-quote-btn");
+const copyBtn = document.getElementById("copy-btn");
+const twitterBtn = document.getElementById("tweet-btn");
+const exportBtn = document.getElementById("export-btn");
+
+async function fetchQuote() {
+  try {
+    const response = await fetch(url);
+    const result = await response.json();
+
+    if (!response.ok) {
+      throw new Error(result.message);
+    }
+
+    console.log(result.data);
+    return result.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function renderQuote({ content, author }) {
+  const seed = Math.floor(Math.random() * 1000);
+
+  quoteText.textContent = content;
+  quoteAuthor.textContent = author;
+  quoteImage.src = "";
+  quoteImage.src = `https://picsum.photos/seed/${seed}/800/600`;
+}
+
+nextQuoteBtn.addEventListener("click", async () => {
+  const quote = await fetchQuote();
+  renderQuote(quote);
+});
+
+copyBtn.addEventListener("click", () => {
+  const text = `"${quoteText.textContent}" ~ ${quoteAuthor.textContent}`;
+  navigator.clipboard.writeText(text);
+
+  const originalText = copyBtn.innerHTML;
+  copyBtn.innerHTML = `
+          <i class="ti ti-check"></i> Copied
+`;
+
+  copyBtn.setAttribute("disabled", true);
+  copyBtn.classList.add("success");
+
+  setTimeout(() => {
+    copyBtn.innerHTML = originalText;
+    copyBtn.removeAttribute("disabled");
+    copyBtn.classList.remove("success");
+  }, 2000);
+});
+
+twitterBtn.addEventListener("click", () => {
+  const text = `"${quoteText.textContent}" ~ ${quoteAuthor.textContent}`;
+  const url = `https://twitter.com/intent/tweet?text=${text}`;
+  window.open(url, "_blank");
+});
+
+exportBtn.addEventListener("click", () => {
+  htmlToImage
+    .toPng(quoteContainer)
+    .then((dataUrl) => {
+      const link = document.createElement("a");
+      link.href = dataUrl;
+      link.download = "quote.png";
+      link.click();
+    })
+    .catch(console.error);
+});
+
+fetchQuote().then(renderQuote);
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Random Quote Generator
+This is a simple random quote generator that displays a random quote from the [ API ](https://api.freeapi.app/api/v1/public/quotes/quote/random). New quotes are displayed on the screen when the user clicks the "Next Quote" button. The background color of the page also changes each time a new quote is displayed.
+
+Deployed site: [Random Quote Generator](https://random-quote-generator-ten.vercel.app/)
+
+### Features
+- [x] Fetch a random quote from the API.
+- [x] Display the quote and author on the screen.
+- [x] Implement a "Next Quote" button that fetches a new quote.
+- [x] Implement a "Copy to Clipboard" button that copies the quote to the clipboard.
+- [x] Add a "Share on Twitter" button that shares the quote on Twitter.
+- [x] Set a random background image when a new quote is displayed.
+- [x] Implement a "Export" button that saves the quotes as an image to the users' device.
+
+### Screenshots
